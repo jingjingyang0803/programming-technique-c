e81069b0d8f1dfaad766997a2e749c9035639e09
Jingjing Yang <jingjing.yang@tuni.fi>
2026-03-21 15:05:18 +0200

    feat(part1): add hello world solution

---
+++ part1-basics/hei.c
@@ -0,0 +1,14 @@
+/*
+EXERCISE: Hello world!
+Implement a program that prints the following text:
+
+Ohjelmoinnin tekniikka
+C-kieli
+Verkkokurssi
+*/
+#include <stdio.h>
+int main(void) {
+  printf("Ohjelmoinnin tekniikka\nC-kieli\nVerkkokurssi\n");
+
+  return 0;
+}
