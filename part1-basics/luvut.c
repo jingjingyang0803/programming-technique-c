eeda0843d539dd857ec6116b3e8bd7b2ebdc785a
Jingjing Yang <jingjing.yang@tuni.fi>
2026-03-21 15:21:12 +0200

    feat(part1): implement program to print non-divisible integers

---
+++ part1-basics/luvut.c
@@ -0,0 +1,31 @@
+/*
+EXERCISE: Numbers
+Implement a program that prints all positive integers in the interval 1, ...,
+100 that are not evenly divisible by two, three or five. You probably want to
+use the modulo operator %: an operation of form a % b calculates the remainder
+of the division a/b. The numbers should be printed in order from smallest to
+largest and they should be separated by single spaces. There must be a line
+break after the last number.
+
+The first ten numbers of the expected ouput are shown below. For example the
+number 10 is not printed because it is divisible by both 2 and 5, and the number
+12 because it is divisible by 3.
+
+1 7 11 13 17 19 23 29 31 37
+*/
+#include <stdio.h>
+int main(void) {
+  const int MAX = 100;
+
+  for (int i = 1; i <= MAX; i++) {
+    if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+      printf("%d ", i);
+    }
+
+    if (i == MAX) {
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
